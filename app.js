12c1a6ee3be40b2364533efa70367739b635730b
webresident <89592473+webresident@users.noreply.github.com>
2021-08-26 17:30:19 +0300

    First Launch

---
+++ app.js
@@ -0,0 +1,72 @@
+$(function () {
+
+    // Fixed Header
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    let nav = $(".nav");
+    let navToggle = $("#navToggle");
+
+    // Функция .on("load") срабатывает невсегда
+    // $(window).on("load", function(){
+    //     console.log("Srabotalo");
+    // });
+
+    changeClass(scrollPos, introH);
+
+    $(window).on("scroll resize", function () {
+        introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        changeClass(scrollPos, introH);
+
+    });
+
+    function changeClass(scrollPos, introH) {
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+    }
+
+
+    // Smooth Scroll
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+    });
+
+    // Nav Toggle
+
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+
+    });
+
+    // Reviews: https://kenwheeler.github.io/slick/
+    let slider = $("#reviewsSlider");
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        dots: true
+    });
+
+
+});
